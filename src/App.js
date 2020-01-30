e9cfdceb82c742b04dcbbedadd58a6d614207ae3
kleber099 <kleber099@gmail.com>
2020-01-30 20:58:57 -0300

    Adicionando imagens e css no webpack

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import './App.css';
+import image from './assets/img.jpg';
+
+function App() {
+  return <img width="200" src={image} />
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
-const soma = (a, b) => a + b;
+import React from 'react';
+import { render } from 'react-dom';
 
-alert(soma(1, 4));
+import App from './App';
+
+render(<App />, document.getElementById('app'));
 
