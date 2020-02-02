8dec0364d8dfb90540235df821f1aee073f11dfd
kleber099 <kleber099@gmail.com>
2020-02-02 10:51:58 -0300

    Adicionando items no array que esta no estado e removendo um item do estado

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 class TechList extends Component {
   state = {
+    newText: '',
     techs: [
       'Node.js',
       'ReactJS',
@@ -9,13 +10,41 @@ class TechList extends Component {
     ]
   };
 
+  handleInputChange = (e) => {
+    this.setState({ newText: e.target.value});
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    this.setState({
+      techs: [ ...this.state.techs, this.state.newText ],
+      newText: '',
+    })
+  }
+
+  handleDelete = (tech) => {
+    this.setState({ techs: this.state.techs.filter(t => t !== tech) });
+  }
+
   render() {
     return (
-      <ul>
-        <li>Node.js</li>
-        <li>ReactJS</li>
-        <li>React Native</li>
-      </ul>
+      <form onSubmit={this.handleSubmit}>
+        <ul>
+          {this.state.techs.map(tech => (
+            <li key={tech}>
+              {tech}
+              <button onClick={() => this.handleDelete(tech)} type="button">Excluir</button>
+            </li>
+          ))}
+        </ul>
+        <input
+          type="text"
+          onChange={this.handleInputChange}
+          value={this.state.newText}
+        />
+        <button type="submit">Enviar</button>
+      </form>
     );
   }
 }
